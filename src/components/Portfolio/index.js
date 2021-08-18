a6ed79be44cb95adc6c624b46e58694dced45f2c
royster <rcantu786@gmail.com>
2021-08-17 22:15:57 -0500

    added more projects to porfolio

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -18,6 +18,24 @@ function Portfolio() {
       repo: "https://github.com/r0yster/Project1"
     },
     {
+      name: 'Book Search',
+      description: 'Application that will allow users to search through google books based on keyword searh. Also feature login functionality allowing the use of saving favorite books along with viewing saved books collection.',
+      link: "https://afternoon-forest-82318.herokuapp.com/",
+      repo: "https://github.com/r0yster/book-search"
+    },
+    {
+      name: 'Budget Tracker',
+      description: 'Budget tracker application to help with expenses as you travel. Track withdrawals and deposits with or without a data/internet connection keeping an accurate account balance',
+      link: "https://sleepy-savannah-50854.herokuapp.com/",
+      repo: "https://github.com/r0yster/challenge-19-budget-tracker"
+    },
+    {
+      name: 'Deep Thoughts',
+      description: 'Application that allows users to share their thoughs and have users react to mentioned thoughts',
+      link: "https://github.com/r0yster/deep-thoughts",
+      repo: "https://github.com/r0yster/deep-thoughts"
+    },
+    {
       name: 'run-buddy',
       description: 'HTML/CSS mock website allowing users to sign up for a run',
       link: "https://r0yster.github.io/run-buddy/",
